d3f1da7d3fcfc3ff705721a477826eea0daf8c45
kmjain999 <kmj5614@psu.edu>
2020-09-18 00:25:13 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,32 @@
+//Author: Karthik Madhav Jain kmj5614@psu.edu
+
+#include <stdio.h>
+#include<readline/readline.h>
+
+int sum_n(int n);
+void print_n(const char* s,int n);
+
+int main(void) {
+  int addition,integer;
+  printf("Enter an int: ");
+  scanf("%d", &integer); 
+  addition = sum_n(integer);
+  printf("sum is %d.",addition);
+  char *string = readline("\nEnter a string: ");
+  print_n(string, integer);
+  return 0;
+}
+
+int sum_n(int n){
+  int value = 0;
+  if(n != 0)
+    value = n + sum_n(n-1);
+    return value;
+}
+
+void print_n(const char*s, int n){
+  if(n > 0)
+    printf("%s\n",s);
+    n--;
+    print_n(s, n);
+}
